69bc085d2bfbaae13ab33402592d17fc5b9eb089
yangtao <yangtao2@xiaoheiban.cn>
2020-05-28 17:36:11 +0800

    mysql conn

--- wordfilter/config.go
+++ wordfilter/config.go
@@ -4,25 +4,34 @@ import (
 	"github.com/jinzhu/gorm"
 	"log"
 	"os"
+	"sync"
 )
 
 type Config struct {
-		Addr      string
-		MysqlConn *gorm.DB
-	}
+	Addr      string
+	MysqlConn *gorm.DB
+}
+
+var once = &sync.Once{}
 
 func NewConfig() *Config {
 	addr := getEnv("ADDR", ":8080")
-	mysql := getEnv("MYSQL", "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local")
-	db, err := gorm.Open("mysql", mysql)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	return &Config{
 		Addr:      addr,
-		MysqlConn: db,
+		MysqlConn: getConn(),
 	}
+}
+func getConn() *gorm.DB {
+	var conn *gorm.DB
+	once.Do(func() {
+		mysql := getEnv("MYSQL", "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local")
+		db, err := gorm.Open("mysql", mysql)
+		if err != nil {
+			log.Fatal(err)
+		}
+		conn = db
+	})
+	return conn
 }
 func getEnv(name string, defaultValue string) string {
 	if len(os.Getenv(name)) > 0 {
